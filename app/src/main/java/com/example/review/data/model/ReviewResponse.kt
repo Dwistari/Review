ca33f9346ca40e898874700e8075f32f6baf0347
dwistari <dwistari16@gmail.com>
2019-02-23 00:00:35 +0700

    data

--- app/src/main/java/com/example/review/data/model/ReviewResponse.kt
+++ app/src/main/java/com/example/review/data/model/ReviewResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 class ReviewResponse(
 
-    @SerializedName("")
+    @SerializedName("requests")
     @Expose
     var review:  MutableList<Review>
 )
